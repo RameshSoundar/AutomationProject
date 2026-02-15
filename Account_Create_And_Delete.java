36e5ae82fa6149bac2061e89667182cc39c1cf8e
RameshSoundar <slramesh20@gmail.com>
2026-02-15 22:20:20 +0530

    Update Account_Create_And_Delete.java
    The Script will create an account for the user with all the details and then delete the account.

--- Account_Create_And_Delete.java
+++ Account_Create_And_Delete.java
@@ -1,5 +1,4 @@
 package Project;
-
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
-
 public class Task1 {
 	WebDriver driver;
 	@Test
@@ -24,20 +22,16 @@ public class Task1 {
 //		Enter details of the user and create the account
 		driver.findElement(By.xpath("//*[@id=\"uniform-id_gender1\"]")).click();
 		driver.findElement(By.xpath("//*[@type='password']")).sendKeys("Ramesh@1207");
-		
 		driver.findElement(By.xpath("//*[@value='20']")).click();
 		driver.findElement(By.xpath("//*[@id='months']/option[4]")).click();
 		driver.findElement(By.xpath("//*[@value='2002']")).click();
-		
 		driver.findElement(By.xpath("//*[@id='newsletter']")).click();
 		driver.findElement(By.xpath("//*[@id='optin']"));
-		
 		driver.findElement(By.xpath("//*[@name='first_name']")).sendKeys("Ramesh");
 		driver.findElement(By.xpath("//*[@name='last_name']")).sendKeys("S");
 		driver.findElement(By.xpath("//*[@name='company']")).sendKeys("Qspiders");
 		driver.findElement(By.xpath("//*[@name='address1']")).sendKeys("189/A,1st Street");
 		driver.findElement(By.xpath("//*[@name='address2']")).sendKeys("Perumalanchi");
-		
 		WebElement option = driver.findElement(By.xpath("//*[@name='country']"));
 		Select select = new Select(option);
 		select.selectByVisibleText("India");
@@ -45,9 +39,7 @@ public class Task1 {
 		driver.findElement(By.xpath("//*[@name='city']")).sendKeys("Tirunelveli");
 		driver.findElement(By.xpath("//*[@name='zipcode']")).sendKeys("627101");
 		driver.findElement(By.xpath("//*[@name='mobile_number']")).sendKeys("7825962115");
-		
 		driver.findElement(By.xpath("//*[@data-qa='create-account']")).click();
-		
 		driver.findElement(By.xpath("//*[@data-qa=\"continue-button\"]")).click();
 //		delete the Account we created
 		driver.findElement(By.xpath("//*[@href=\"/delete_account\"]")).click();
